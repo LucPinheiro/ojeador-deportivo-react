e1fca35655c6dddc245fd7fa01cc2f0b8ba2b88c
Lu Pinheiro <79840324+LucPinheiro@users.noreply.github.com>
2026-05-29 16:46:19 +0200

    Update App.js

--- f11talents/src/App.js
+++ f11talents/src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import React from 'react';
-import { LoginForm } from './components/LoginForm';
+import React, { useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-// import { get } from './api/api';
-
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Search from './components/pages/Search';
@@ -13,89 +9,88 @@ import Login from './components/pages/Login';
 import AboutUs from './components/pages/AboutUs';
 import ContactUs from './components/pages/ContactUs';
 import MyAccount from './components/pages/MyAccount';
-import { useContext } from 'react/cjs/react.development';
+
 import { AuthTokenContext } from '.';
 
-// function App() {
-// useEffect(() => {
-//   get('http://localhost:4000/profiles', (body) => setProfilesList(body));
-// }, []);
-// <Router>
-//   {/* <Navbar />*/}
-//   <Switch>
-//     {/* <Route path='/' exact component={Home} /> */}
-//     <Route path='/busqueda' component={Search} />
-//     {/* <Route path='/nosotros' component={AboutUs} />
-//   <Route path='/contactos' component={ContactUs} />
-//   <Route path='/miCuenta' component={MyAccount} /> */}
-//     {/* <Route path='/registar' component={FormularioRegistro} /> */}
-//   </Switch>
-//   {/*<Footer /> */}
-// </Router>;
-// }
-
 function App() {
-  const { token, user, logout } = useContext(AuthTokenContext);
+  const auth = useContext(AuthTokenContext);
+
+  const token = auth?.token;
+  const logout = auth?.logout;
+
   return (
     <Router>
       <div>
         <nav>
           <ul>
             <li>
-              <Link to='/'>Início</Link>
+              <Link to="/">Inicio</Link>
             </li>
+
             <li>
-              <Link to='/buscar'>Búsqueda</Link>
+              <Link to="/buscar">Búsqueda</Link>
             </li>
+
             <li>
-              <Link to='/micuenta'>Mi Cuenta</Link>
+              <Link to="/micuenta">Mi Cuenta</Link>
             </li>
+
             <li>
-              <Link to='/about'>Sobre esta página</Link>
+              <Link to="/about">Sobre esta página</Link>
             </li>
+
             <li>
-              <Link to='/contact'>Contacto</Link>
+              <Link to="/contact">Contacto</Link>
             </li>
           </ul>
 
           <div>
             {token ? (
               <p>
-                Estás logueado <button onClick={() => logout()}>Log out</button>
+                Estás logueado{' '}
+                <button onClick={logout}>Log out</button>
               </p>
             ) : (
               <ul>
                 <li>
-                  <Link to='/login'>Login</Link>
+                  <Link to="/login">Login</Link>
                 </li>
+
                 <li>
-                  <Link to='/register'>Registro</Link>
+                  <Link to="/register">Registro</Link>
                 </li>
               </ul>
             )}
           </div>
         </nav>
+
         <Switch>
-          <Route path='/buscar'>
+          <Route exact path="/">
+            <Home />
+          </Route>
+
+          <Route path="/buscar">
             <Search />
           </Route>
-          <Route path='/micuenta'>
+
+          <Route path="/micuenta">
             <MyAccount />
           </Route>
-          <Route path='/register'>
+
+          <Route path="/register">
             <Register />
           </Route>
-          <Route path='/login'>
+
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/about'>
+
+          <Route path="/about">
             <AboutUs />
           </Route>
-          <Route path='/contact'>
+
+          <Route path="/contact">
             <ContactUs />
-          </Route>
-          <Route path='/'>
-            <Home />
           </Route>
         </Switch>
       </div>
